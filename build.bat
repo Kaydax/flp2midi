69442ae68f80909bc9684201c3fb0648345e1fb1
Kaydax <kaydax@kaydax.xyz>
2021-02-12 10:01:05 -0500

    Add auto archiving to the build scripts because im lazy

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ ECHO Cleaning up old builds...
 rmdir /s /q bin\x64\Release
 rmdir /s /q bin\Any\Release
 rmdir /s /q obj
+rmdir /s /q builds
 
 ECHO Updating externals...
 git submodule init
@@ -11,9 +12,18 @@ git submodule update
 
 ECHO Building...
 dotnet publish -p:PublishProfile="Windows - Release x64"
-dotnet publish -p:PublishProfile="Windows - Release x32"
+dotnet publish -p:PublishProfile="Windows - Release x86"
 dotnet publish -p:PublishProfile="Windows - Release Arm"
 dotnet publish -p:PublishProfile="Linux - Release x64"
 dotnet publish -p:PublishProfile="Linux - Release Arm"
 dotnet publish -p:PublishProfile="OSX - Release x64"
+
+ECHO Archiving...
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-win-x64.7z .\bin\x64\Release\net5.0\publish\win-x64\*.dll .\bin\x64\Release\net5.0\publish\win-x64\flp2midi.exe
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-win-x86.7z .\bin\Any\Release\net5.0\publish\win-x86\*.dll .\bin\Any\Release\net5.0\publish\win-x86\flp2midi.exe
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-win-arm.7z .\bin\Any\Release\net5.0\publish\win-arm\*.dll .\bin\Any\Release\net5.0\publish\win-arm\flp2midi.exe
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-linux-x64.7z .\bin\x64\Release\net5.0\publish\linux-x64\flp2midi
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-linux-arm.7z .\bin\Any\Release\net5.0\publish\linux-arm\flp2midi
+7z a -t7z -mmt16 -mx9 .\builds\flp2midi-osx-x64.7z .\bin\x64\Release\net5.0\publish\osx-x64\flp2midi .\bin\x64\Release\net5.0\publish\osx-x64\*.dylib
+
 PAUSE
